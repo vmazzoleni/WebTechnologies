aefd3df30546e54964c630370757cf55180fc855
vmazzoleni <vanessa.mazzoleni@zhdk.ch>
2026-06-04 13:17:36 +0200

    java array

--- exercises/arrays/script.js
+++ exercises/arrays/script.js
@@ -10,13 +10,15 @@
 const ingredients = ["Tomatoes", "Onions", "Garlic", "Olive oil", "Salt"];
 
 // TODO 1: log the entire array
-console.log(/* ??? */);
+console.log(ingredients);
 
 // TODO 2: log just the first ingredient
-console.log(/* ??? */);
+console.log(ingredients[0]);
 
 // TODO 3: log how many ingredients there are
-console.log(/* ??? */);
+console.log(ingredients.length);
 
 // TODO 4: loop over the array and log each ingredient one by one
-/* ??? */
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
