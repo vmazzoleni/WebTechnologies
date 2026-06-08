5d5e6daf32b6f980269891fab8540782abc928e4
vmazzoleni <vanessa.mazzoleni@zhdk.ch>
2026-06-08 11:40:33 +0200

    new readme page

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Webtechnologies module as part of the interaction design study, 2026.
 
-## Slides
+## 
 
-- [2026-06-01](https://downloads.jonasscheiwiller.ch/2026_Webtech_01.pdf)
+Here are my exercices from the Webtechnologies module.
+Main page should open all exercice overview!
+
--- examples/script.js
+++
@@ -1,8 +0,0 @@
-// This file is linked from 11-link-css-js.html
-
-const button = document.getElementsByTagName("button")[0];
-const message = document.getElementById("message");
-
-button.addEventListener("click", function () {
-  message.textContent = "JavaScript changed this text!";
-});
